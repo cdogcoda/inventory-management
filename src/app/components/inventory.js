9616c36a4124661b1e94256cc22fa82b04f1eca2
cdog <cdog97456@gmail.com>
2024-08-01 22:09:37 -0700

    Add styling and functionality to tracker
    Styling is achieved by the use of MUI themeProvider
    and functionality of the tracker includes adding,
    removing, and searching for items.

---
+++ src/app/components/inventory.js
@@ -0,0 +1,59 @@
+'use client'
+
+import { Box, Typography, Button } from '@mui/material'
+
+function Inventory({ inventory, theme, removeItem }) {
+    if (inventory.length) {
+      return (inventory.map(({name, quantity}) => (
+        <Box
+          key={name}
+          width='100%'
+          minHeight='150px'
+          display={'flex'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          bgcolor={'#f0f0f0'}
+          padding={'24px'}
+          borderTop={'2px solid black'}
+        >
+          <Typography
+            variant='h3'
+            color={'#333'}
+            textAlign={'center'}
+          >
+            {name.charAt(0).toUpperCase() + name.slice(1)}
+          </Typography>
+          <Typography
+            variant='h3'
+            color={'#333'}
+            textAlign={'center'}
+          >
+            Quantity: {quantity}
+          </Typography>
+          <Button
+            theme={theme}
+            variant="pretty"
+            onClick={() => {
+              removeItem(name)
+            }}
+          >
+            Remove 
+          </Button>
+        </Box>
+      )))
+    } else {
+      return (
+        <Typography
+          variant='h3'
+          color={'#333'}
+          textAlign={'center'}
+          alignSelf={'center'}
+        >
+          No logged items
+        </Typography>
+      )
+    }
+  }
+
+
+export { Inventory }
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,325 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// src/page.js
+
+'use client';
+
+import { useState, useEffect } from "react"
+import { Box, Stack, Typography, Button, Modal, TextField, createTheme, ThemeProvider, createMuiTheme } from '@mui/material'
+import { firestore } from './firebase'
+import { collection, doc, getDocs, query, setDoc, deleteDoc, getDoc } from 'firebase/firestore'
+import { Inventory } from './components/inventory'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          textShadow: '0px 0px 2px rgba(0,0,0,.3)'
+        }, 
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textShadow: '0px 0px 2px rgba(0,0,0,.3)'
+        }
+      },
+      variants: [{
+        props: {variant: 'pretty'},
+        style: {
+          backgroundColor: 'pink',
+          '&:hover': {
+            backgroundColor: 'lightpink'
+          }
+        }
+      }]
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiOutlinedInput-root': {
+            '&.Mui-focused fieldset': {
+              borderColor: 'pink',
+              color: 'pink'
+            }
+          },
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          '&.Mui-focused': {
+            color: '#cb416b'
+          }
+        }
+      }
+    },
+  }
+})
+
+
 
 export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+  const [searchItemName, setSearchItemName] = useState('')
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data()})
+    })
+
+    console.log(inventoryList)
+
+    setInventory(inventoryList)
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+  const addItem = async (itemName) => {
+    if (!itemName) {
+      alert("Please add a valid item.")
+      return
+    }
+
+    const item = itemName.toLowerCase();
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1})
+    } else {
+      await setDoc(docRef, { quantity: 1 })
+    }
+    await updateInventory()
+
+  }
+
+  const removeItem = async (itemName) => {
+    const item = itemName.toLowerCase();
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (searchItemName) {
+      const snapshot = query(collection(firestore, 'inventory'))
+      const docs = await getDocs(snapshot)
+      const docNames = Array.from(docs.docs).map((doc) => {
+        return doc.id
+      })
+      const searchedInventory = []
+      
+      const matchedDocs = docNames.filter((doc) => {
+        return (doc.includes(searchItemName))
+      }).map(async (match) => {
+        return await getDoc(doc(collection(firestore, 'inventory'), match))
+      })
+
+      for (const doc of matchedDocs) {
+        const name = (await doc).id
+        const quantity = (await doc).data()['quantity']
+        const itemLog = {}
+        if (name === item && quantity === 1) {
+          await deleteDoc(docRef)
+          continue
+        } else if (name === item && quantity > 1) {
+          await setDoc(docRef, { quantity: quantity - 1 })
+          itemLog['name'] = name
+          itemLog['quantity'] = quantity - 1
+        } else {
+          itemLog['name'] = name
+          itemLog['quantity'] = quantity
+        }
+        
+        searchedInventory.push(itemLog)
+      }
+
+      setInventory(searchedInventory)
+    } else {
+        if (docSnap.exists()) {
+          const { quantity } = docSnap.data()
+          if (quantity === 1) {
+            await deleteDoc(docRef)
+          } else {
+            await setDoc(docRef, { quantity: quantity - 1 })
+          }
+        }
+        await updateInventory();
+    }
+      
+  }
+
+  const searchItem = async (itemName) => {
+    const item = itemName.toLowerCase();
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const docNames = Array.from(docs.docs).map((doc) => {
+      return doc.id
+    })
+    const searchedInventory = []
+    
+    const matchedDocs = docNames.filter((doc) => {
+      return (doc.includes(item))
+    }).map(async (match) => {
+      return await getDoc(doc(collection(firestore, 'inventory'), match))
+    })
+
+    for (const doc of matchedDocs) {
+      const itemLog = {}
+      itemLog['name'] = (await doc).id
+      itemLog['quantity'] = (await doc).data()['quantity']
+      searchedInventory.push(itemLog)
+    }
+
+    setInventory(searchedInventory)
+
+  }
+
+    
+  
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Box
+      width='100vw'
+      height='100vh'
+      display={'flex'}
+      justifyContent={'center'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      gap={2}
+      bgcolor={'#FEEEED'}
+    >
+      <Modal
+        theme={theme}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box
+          sx={style} 
+        >
+          <Typography
+            id='modal-modal-title'
+            variant='h6'
+            component='h2'
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+            Add Item
+          </Typography>
+          <Stack
+            width='100%'
+            direction={'row'}
+            spacing={2}
+          >
+            <TextField
+              id='outlined-basic'
+              label='Item'
+              variant='outlined'
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+            <Button
+              theme={theme}
+              variant="pretty"
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Stack
+        width={'800px'}
+        direction={'row'}
+        display={'flex'}
+        justifyContent={'space-between'}
+      >
+        <Button
+          theme={theme}
+          variant="pretty"
+          onClick={handleOpen}
+        >
+          Add
+        </Button>
+        <TextField 
+          theme={theme}
+          id="outlined-basic" 
+          label="Search" 
+          value={searchItemName}
+          onChange={(e) => {
+            setSearchItemName(e.target.value)
+            searchItem(e.target.value)
+          }}
+          
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      </Stack>
+      
+      <Box
+        borderRadius={'24px'}
+        boxShadow={'0px 0px 12px rgba(0,0,0,.25)'}
+      >
+        <Box
+          width='800px'
+          height='100px'
+          bgcolor={'lightpink'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          borderRadius={'24px 24px 0px 0px'}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          <Typography
+            variant='h2'
+            color={'#333'}
+            textAlign={'center'}
+          >
+            Inventory Items
+          </Typography>
+        </Box>
+        <Stack
+          width='800px'
+          height='400px'
+          spacing={0}
+          overflow={'auto'}
+          borderRadius={'0px 0px 24px 24px'}
+          display={'flex'}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+          <Inventory inventory={inventory} theme={theme} removeItem={removeItem}></Inventory>
+        </Stack>
+      </Box>
+    </Box>
+    </ThemeProvider>
+    
+  )
 }
